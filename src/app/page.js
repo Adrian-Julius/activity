8fcd3fb0e1b7c4305678b60bf60a3981a2fd1c66
Adrian-Julius <adrianvillaruel2223@gmail.com>
2024-11-29 18:03:02 +0800

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,77 @@
+"use client";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  // 1. Create a counter app (Same as activity in worksheet)
+  // 2. Explain what's happening
+  //     1. Increment by plus 1
+  //     2. Decrement by minus 1
+  //     3. Reset to 0
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  // Section 2:
+  // Once above is complete:
+  // 1. Modify the counter app
+  //     1. You may change initial state in this section
+  //     2. Increment by times 2
+  //     3. Decrement by divided by 2
+  //     4. Reset to 1
+
+  // Section 3:
+  // Add Supabase auth
+  // 1. Create log in page
+  // 2. Access counter app only when logged in
+  // 3. Redirect to login page if not user
+
+  // Section 4:
+  // Deploy to Vercel
+  const [count, setCount] = useState(1);
+
+  const [isLogin, setIsLogin] = useState(false);
+
+
+
+  return (
+    // <div className="text-center">
+    //   <h1>{count}</h1>
+    //   <button
+    //     onClick={() => setCount(count - 1)}
+    //     className="p-3 m-5 bg-slate-200"
+    //   >
+    //     DECREMENT(-1)
+    //   </button>
+    //   <button onClick={() => setCount(0)} className="p-3 m-5 bg-slate-200">
+    //     RESET (0)
+    //   </button>
+    //   <button
+    //     onClick={() => setCount(count + 1)}
+    //     className="p-3 m-5 bg-slate-200"
+    //   >
+    //     INCREMENT (+1)
+    //   </button>
+    // </div>
+
+      // <div className="text-center">
+      //   <h1>{count}</h1>
+      //   <button
+      //     onClick={() => setCount(count / 2)}
+      //     className="p-3 m-5 bg-slate-200"
+      //   >
+      //     DECREMENT(-1)
+      //   </button>
+      //   <button onClick={() => setCount(1)} className="p-3 m-5 bg-slate-200">
+      //     RESET (1)
+      //   </button>
+      //   <button
+      //     onClick={() => setCount(count * 2)}
+      //     className="p-3 m-5 bg-slate-200"
+      //   >
+      //     INCREMENT (+1)
+      //   </button>
+      // </div>
+    
+    <div>
+      h1
     </div>
   );
 }
